fc53cbd9c57f76d62f08710c1a8707537261015d
ccroww <cameron@ccroww.com>
2024-07-14 14:58:22 -0700

    modified:   index.js 	modified:   package.json 	credentials/ 	nodes/

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    nodes: [
+            require('./dist/nodes/Nexrender/Nexrender.node.js'),
+    ],
+    credentials: [
+            require('./dist/credentials/NexrenderApi.credentials.js'),
+    ],
+};
