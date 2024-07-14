156072e542f7202fee1483b1806dead793bb9cc6
ccroww <cameron@ccroww.com>
2024-07-14 12:42:03 -0700

    deleted:    LICENSE 	deleted:    Nexrender.node.json 	modified:   README.md 	deleted:    credentials/NexrenderApi.credentials.ts 	modified:   gulpfile.js 	deleted:    icon.png 	modified:   index.js 	deleted:    nexrender-api/package.json 	deleted:    nexrender-api/src/index.d.ts 	deleted:    nexrender-api/src/index.js 	deleted:    nexrender-api/src/job.js 	deleted:    nexrender-api/test/index.js 	deleted:    nexrender-api/test/manual.js 	deleted:    nodes/Nexrender/Nexapi.d.ts 	deleted:    nodes/Nexrender/Nexapi.node.ts 	deleted:    nodes/Nexrender/Nexapi/index.js 	deleted:    nodes/Nexrender/Nexapi/job.js 	deleted:    nodes/Nexrender/Nexrender.node.ts 	deleted:    nodes/Nexrender/icon.png 	modified:   package.json 	modified:   pnpm-lock.yaml 	deleted:    tsc 	modified:   tsconfig.json
    	README_TEMPLATE.md
    	credentials/
    	nodes/
    
    fuck this shit.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,16 @@
 const path = require('path');
-const { task, src, dest, series } = require('gulp');
+const { task, src, dest } = require('gulp');
 
-task('build:icons', copyNodesIcons);
-task('build', series(copyNodesIcons));
+task('build:icons', copyIcons);
 
-function copyNodesIcons() {
+function copyIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
 	const nodeDestination = path.resolve('dist', 'nodes');
-	src(nodeSource).pipe(dest(nodeDestination));
 
-	const apiSource = path.resolve('nodes', 'Nexrender', 'Nexapi', '**', '*');
-	const apiDestination = path.resolve('dist', 'nodes', 'Nexrender', 'Nexapi');
-	src(apiSource).pipe(dest(apiDestination));
+	src(nodeSource).pipe(dest(nodeDestination));
 
 	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
 	const credDestination = path.resolve('dist', 'credentials');
 
 	return src(credSource).pipe(dest(credDestination));
 }
-
--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	nodes: [
-			require('./dist/nodes/Nexrender/Nexrender.node.js'),
-	],
-	credentials: [
-			require('./dist/credentials/NexrenderApi.credentials.js'),
-	],
-};
